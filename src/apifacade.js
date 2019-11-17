b31053b37e47ca5593a9bda31a791172bb197ad4
unknown <cph-md273@cphbusiness.dk>
2019-11-17 18:03:57 +0100

    changed domain

--- src/apifacade.js
+++ src/apifacade.js
@@ -1,4 +1,4 @@
-const URL = "http://localhost:8080/securitystarter";
+const URL = "http://martindoi.dk/CA3";
 function handleHttpErrors(res) {
     if (!res.ok) {
         return Promise.reject({ status: res.status, fullError: res.json() })
